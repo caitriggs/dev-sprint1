e0199a6b0648be5c333124db801ceefc2d86f83d
Caitlin Riggs <caitriggs@gmail.com>
2014-04-17 18:57:39 -0400

    finish sprint1

--- exercise54.py
+++ exercise54.py
@@ -1,2 +1,18 @@
 # This is where Exercise 5.4 goes
-# Name:
+# Name: Cait
+
+a = int(input('Enter 1st side length to check if real triangle: '))
+b = int(input('Enter 2nd side length to check if real triangle: '))
+c = int(input('Enter 3rd side length to check if real triangle: '))
+
+def is_triangle(a, b, c):
+	if a > b + c:
+		print 'No'
+	elif b > a + c:
+		print 'No'
+	elif c > a + b:
+		print 'No'
+	else:
+		print 'Yes'	
+
+is_triangle(a, b, c)		
---
+++ swampy-package/swampy/exercise56.py
@@ -0,0 +1,41 @@
+# Name: Cait
+
+
+#http://www.greenteapress.com/thinkpython/code/koch.py
+
+from TurtleWorld import *
+import math
+
+
+def koch(t, n):
+	if n < 3:
+		fd(t, n)
+		return
+	m = n / 3.0
+	koch(t, m)
+	lt(t, 60)
+	koch(t, m)
+	rt(t, 120)
+	koch(t, m)
+	lt(t, 60)	
+	koch(t, m)
+
+def snowflake(t, n):
+    for i in range(3): #3 sided
+        koch(t, n)
+        rt(t, 120)
+
+
+world = TurtleWorld()
+bob = Turtle()
+bob.delay = 0
+
+#offset start point
+bob.x = -150
+bob.y = 90
+bob.redraw()
+
+snowflake(bob, 300)
+
+
+world.mainloop()	
